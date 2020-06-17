ab678d4d889131a0de326bb1327d68af9e17b260
YuLin <r08522622@ntu.edu.tw>
2020-06-17 14:32:51 +0800

    Update Odom Node

--- twist2odom.py
+++ twist2odom.py
@@ -17,7 +17,7 @@ class Twist2Odom():
         self.w = 0
 
         self.last_time = rospy.Time.now()
-        rospy.Subscriber('/STM32_twist', Twist, self.get_cmd_vel)
+        rospy.Subscriber('/STM32_twist', Twist, self.get_twist)
 
         # while not rospy.is_shutdown():
             
@@ -27,24 +27,24 @@ class Twist2Odom():
         self.current_time = rospy.Time.now()
         self.v = cmd.linear.x
         self.w = cmd.angular.z
-        self.dt = (self.current_time - self.last_time).to_sec()
-        self.delta_th = self.w * self.dt
-        self.th += self.delta_th
-        self.vx = self.v * cos(self.th)
-        self.vy = self.v * sin(self.th)
+        # self.dt = (self.current_time - self.last_time).to_sec()
+        # self.delta_th = self.w * self.dt
+        # self.th += self.delta_th
+        # self.vx = self.v * cos(self.th)
+        # self.vy = self.v * sin(self.th)
 
 
-        self.odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
+        self.odom_quat = tf.transformations.quaternion_from_euler(0, 0, 0)
 
         self.odom = Odometry()
         self.odom.header.stamp = self.current_time
         self.odom.header.frame_id = "odom"
         self.odom.pose.pose = Pose(Point(0, 0, 0.), Quaternion(*self.odom_quat))
         self.odom.child_frame_id = "base_link"
-        self.odom.twist.twist = Twist(Vector3(self.vx, self.vy, 0), Vector3(0, 0, self.w))
+        self.odom.twist.twist = Twist(Vector3(self.v, 0, 0), Vector3(0, 0, self.w))
         self.odom_pub.publish(self.odom)
         rospy.loginfo(self.odom)
-        self.last_time = self.current_time
+        # self.last_time = self.current_time
 
 if __name__ == '__main__':
     
